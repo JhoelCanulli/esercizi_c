a34eb5d31e95d6d02e60696534496a3e8b9baf52
Jhoel Canulli <115298625+JhoelCanulli@users.noreply.github.com>
2023-03-05 20:05:17 +0100

    Update tris.c

--- tris.c
+++ tris.c
@@ -1,151 +1,197 @@
-/* crea il gioco del tris in C PvP */
+/* gioco del tris pvp */
 
 #include <stdio.h>
 #include <stdbool.h>
 
 #define SIZE 3
 
-// Funzione che stampa la griglia di gioco
-void print_board(char board[][SIZE]) {
-  for (int i = 0; i < SIZE; i++) {
-    for (int j = 0; j < SIZE; j++) {
-      printf("%c", board[i][j]);
-      if (j < SIZE - 1) {
-        printf("|");
-      }
+void print_board(char board[][SIZE]);
+bool check_win(char board[][SIZE], char player);
+void count_win(char board[][SIZE], int *x_wins, int *o_wins);
+
+int main(){
+    
+    char player = 'X';  //Inizializzazione giocatore
+
+    // Inizializzazione numero vincite dei giocatori
+    int x_wins = 0;
+    int o_wins = 0;
+    char input;
+
+    printf("*-----------*\n");
+    printf("|   TRIS    |\n");
+    printf("*-----------*\n\n");
+    
+    while(true){
+
+        // Inizializzazione griglia di gioco vuota 
+        char board[SIZE][SIZE];
+            for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    board[i][j] = ' ';
+                }
+            }
+        printf("\n[digitare q per uscire dal gioco, qualsiasi altro carattere per continuare]\n");
+        scanf("%c",&input);
+        while (getchar() != '\n');
+
+        if(input == 'q'){
+            break;
+        }
+
+        while(true){
+            
+            print_board(board);
+
+            // Giocatore muove
+            int row, col;
+            printf("Turno del giocatore %c\n", player);
+            printf("Inserire riga e colonna (es. 1 2): ");
+            scanf("%d %d", &row, &col);
+
+            // Verifica validità mossa
+            if (row < 1 || row > SIZE || col < 1 || col > SIZE) {
+                printf("Mossa non valida, inserisci riga e colonna tra 1 e %d\n", SIZE);
+                continue;
+            }
+            if (board[row - 1][col - 1] != ' ') {
+                printf("Mossa non valida, la casella è già occupata\n");
+                continue;
+            }
+
+            board[row - 1][col -1] = player;  // Esecuzione mossa giocatore
+
+            //Verifica se il giocatore ha vinto
+            bool win = check_win(board, player);
+            if(win){
+                // Aggiornamento conteggio vittorie
+                count_win(board, &x_wins, &o_wins);
+                printf("\nIl giocatore %c ha vinto!!!!!\n", player);
+                break;
+            }
+
+            //Verifica se si ha un pareggio
+            bool full_board = true;
+            for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    if (board[i][j] == ' ') {
+                        full_board = false;
+                        break;
+                    }
+                }
+                if (!full_board) {
+                break;
+                }
+            }
+            if (full_board) {
+                printf("\nPareggio!\n");
+                break;
+            }
+
+            //Tocca al giocatore successivo
+            if (player == 'X') {
+                player = 'O';
+            } else {
+                player = 'X';
+            }
+
+        }
+      
+        // Stampa dei risultati
+        printf("\nNumero di vittorie di X: %d", x_wins);
+        printf("\nNumero di vittorie di O: %d\n\n", o_wins);
+
     }
-    printf("\n");
-    if (i < SIZE - 1) {
-      printf("-+-+-\n");
-    }
-  }
+
+    return 0;
 }
 
-// Funzione che controlla se c'è una vittoria per il giocatore "player"
-bool check_win(char board[][SIZE], char player) {
-  // Controllo delle righe
-  for (int i = 0; i < SIZE; i++) {
-    bool win = true;
-    for (int j = 0; j < SIZE; j++) {
-      if (board[i][j] != player) {
-        win = false;
-        break;
-      }
-    }
-    if (win) {
-      return true;
-    }
-  }
 
-  // Controllo delle colonne
-  for (int i = 0; i < SIZE; i++) {
-    bool win = true;
-    for (int j = 0; j < SIZE; j++) {
-      if (board[j][i] != player) {
-        win = false;
-        break;
-      }
-    }
-    if (win) {
-      return true;
-    }
-  }
+void print_board(char board[][SIZE]){
 
-  // Controllo delle diagonali
-  bool win = true;
-  for (int i = 0; i < SIZE; i++) {
-    if (board[i][i] != player) {
-      win = false;
-      break;
-    }
-  }
-  if (win) {
-    return true;
-  }
-
-  win = true;
-  for (int i = 0; i < SIZE; i++) {
-    if (board[i][SIZE - 1 - i] != player) {
-      win = false;
-      break;
-    }
-  }
-  if (win) {
-    return true;
-  }
-
-  // Nessuna vittoria trovata
-  return false;
-}
-
-int main() {
-  // Inizializziamo la griglia di gioco vuota
-  char board[SIZE][SIZE];
-  for (int i = 0; i < SIZE; i++) {
-    for (int j = 0; j < SIZE; j++) {
-      board[i][j] = ' ';
-    }
-  }
-
-  // Giocatore iniziale (X)
-  char current_player = 'X';
-
-  // Ciclo di gioco
-  while (true) {
-    // Stampa la griglia di gioco
-    print_board(board);
-
-    // Chiedi al giocatore corrente di fare una mossa
-    int row, col;
-    printf("Turno del giocatore %c\n", current_player);
-    printf("Inserisci riga e colonna (es. 1 2): ");
-    scanf("%d %d", &row, &col);
-
-    // Verifica che la mossa sia valida
-    if (row < 1 || row > SIZE || col < 1 || col > SIZE) {
-      printf("Mossa non valida, inserisci riga e colonna tra 1 e %d\n", SIZE);
-      continue;
-    }
-    if (board[row - 1][col - 1] != ' ') {
-        printf("Mossa non valida, la casella è già occupata\n");
-        continue;
-    }
-
-    // Esegui la mossa
-    board[row - 1][col - 1] = current_player;
-
-    // Verifica se il giocatore ha vinto
-    if (check_win(board, current_player)) {
-        printf("Il giocatore %c ha vinto!\n", current_player);
-        break;
-    }
-
-    // Verifica se c'è un pareggio
-    bool full_board = true;
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
-            if (board[i][j] == ' ') {
-                full_board = false;
+            printf("%c", board[i][j]);
+            
+            if (j < SIZE - 1) {
+                printf("|");
+            }
+        }
+        printf("\n");
+        if (i < SIZE - 1) {
+            printf("-+-+-\n");
+        }
+    }
+}
+
+bool check_win(char board[][SIZE], char player){
+    
+    // Vincita su riga
+    for (int i = 0; i < SIZE; i++) {
+        bool win = true;
+        for (int j = 0; j < SIZE; j++) {
+            if (board[i][j] != player) {
+                win = false;
                 break;
             }
         }
-        if (!full_board) {
-        break;
+        if (win) {
+            return true;
         }
     }
 
-    if (full_board) {
-        printf("Pareggio!\n");
-        break;
+    // Vincita su colonna
+    for (int i = 0; i < SIZE; i++) {
+        bool win = true;
+        for (int j = 0; j < SIZE; j++) {
+            if (board[j][i] != player) {
+                win = false;
+                break;
+            }
+        }
+        if (win) {
+            return true;
+        }
     }
 
-    // Passa al giocatore successivo
-    if (current_player == 'X') {
-        current_player = 'O';
-    } else {
-        current_player = 'X';
+    // Vincita su diagonale principale
+    bool win = true;
+    for (int i = 0; i < SIZE; i++) {
+        if (board[i][i] != player) {
+            win = false;
+            break;
+        }
     }
-  }
+    if (win) {
+        return true;
+    }
 
-return 0;
+    // Vincita su diagonale secondaria
+    win = true;
+    for (int i = 0; i < SIZE; i++) {
+        if (board[i][SIZE - 1 - i] != player) {
+            win = false;
+            break;
+        }
+    }
+    if (win) {
+        return true;
+    }
+
+    // Nessuna vittoria trovata
+    return false;
+}
+
+void count_win(char board[][SIZE], int *x_wins, int *o_wins)
+{
+    bool x_won = check_win(board, 'X');
+    bool o_won = check_win(board, 'O');
+    
+    if (x_won || o_won) {
+        if (x_won) {
+            (*x_wins)++;
+        } else if (o_won) {
+            (*o_wins)++;
+        }
+    }
 }
